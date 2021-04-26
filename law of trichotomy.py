ba0749962cc05d59a436c9a55edc2abe558ba5ac
Omkar Gharat <83211956+OmkarGharat@users.noreply.github.com>
2021-04-26 15:26:27 +0530

    Add files via upload

---
+++ law of trichotomy.py
@@ -0,0 +1,11 @@
+# law of trichotomy
+
+a = int(input('Enter 1st integer : '))
+b = int(input('Enter 2nd integer : '))
+
+if a<b :
+    print(a,'is less than ',b)
+elif a > b :
+    print(a,'is greater than ',b)
+else :
+    print(a , 'is equal to ', b)
